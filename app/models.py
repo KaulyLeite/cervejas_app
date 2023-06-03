383f05110b4adf89bb691a95dba51f6c4c28fe73
Kauly <kauly.leite@icloud.com>
2023-06-03 20:54:44 -0300

    refactor: Refatorando o conteúdo e o estilo da página principal. Ajustando o posicionamento das imagens das marcas.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Marca(models.Model):
+    objects = models.Manager()
     nome = models.CharField(max_length=30)
     descricao = models.TextField()
     imagem = models.ImageField(upload_to='images')
