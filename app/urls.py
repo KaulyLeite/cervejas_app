d2064a8c93fc5f5b141e9a52668555ecb23e10ca
Kauly <kauly.leite@icloud.com>
2023-06-24 18:36:42 -0300

    feat: Implementando a página de confirmação dos dados para o envio do pedido.

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-                  path('', views.index, name="home"),
-                  path('produtos', views.produtos, name="produtos"),
-                  path('sobre', views.sobre, name="sobre"),
-                  path('pedidos', views.pedidos, name="pedidos"),
+                  path('', views.index, name='home'),
+                  path('produtos', views.produtos, name='produtos'),
+                  path('sobre', views.sobre, name='sobre'),
+                  path('pedidos', views.pedidos, name='pedidos'),
+                  path('envio', views.envio, name='envio'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-
 from app.models import Marca, Produto
+from datetime import datetime
+from django.shortcuts import render
+import uuid
 
-TEMPLATE_BASE = "main/base.html"
-TEMPLATE_PRODUTOS = "main/produtos.html"
-TEMPLATE_SOBRE = "main/sobre.html"
-TEMPLATE_PEDIDOS = "main/pedidos.html"
+TEMPLATE_BASE = 'main/base.html'
+TEMPLATE_PRODUTOS = 'main/produtos.html'
+TEMPLATE_SOBRE = 'main/sobre.html'
+TEMPLATE_PEDIDOS = 'main/pedidos.html'
+TEMPLATE_ENVIO = 'main/envio.html'
 
 
 # Create your views here.
@@ -15,8 +17,8 @@ def index(request):
 
 
 def produtos(request):
-    produtos = Produto.objects.all()
-    return render(request, TEMPLATE_PRODUTOS, {'produtos': produtos})
+    produtos_listados = Produto.objects.all()
+    return render(request, TEMPLATE_PRODUTOS, {'produtos': produtos_listados})
 
 
 def sobre(request):
@@ -24,5 +26,37 @@ def sobre(request):
 
 
 def pedidos(request):
-    produtos = Produto.objects.all()
+    produtos_pedidos = Produto.objects.all()
+    return render(request, TEMPLATE_PEDIDOS, {'produtos': produtos_pedidos})
+
+
+def envio(request):
+    produtos_enviados = Produto.objects.all()
+    if request.method == 'POST':
+        pedido = uuid.uuid4().hex[:8].upper()
+
+        nome = request.POST['nome']
+        email = request.POST['email']
+        telefone = request.POST['telefone']
+        endereco = request.POST['endereco']
+        cidade = request.POST['cidade']
+        estado = request.POST['estado']
+
+        data = datetime.now().strftime('%d/%m/%Y')
+
+        produtos_selecionados = []
+        for produto in produtos_enviados:
+            qtde_key = f'{produto.id}_qtde'
+            qtde = int(request.POST.get(qtde_key, 0))
+            if qtde > 0:
+                produto.total = produto.valor * qtde
+                produto.qtde = qtde
+                produtos_selecionados.append(produto)
+
+        total_pedido = sum(produto.total for produto in produtos_selecionados)
+
+        return render(request, TEMPLATE_ENVIO,
+                      {'pedido': pedido, 'nome': nome, 'email': email, 'telefone': telefone,
+                       'endereco': endereco, 'cidade': cidade, 'estado': estado, 'data': data,
+                       'produtos': produtos_selecionados, 'total_pedido': total_pedido})
     return render(request, TEMPLATE_PEDIDOS, {'produtos': produtos})
