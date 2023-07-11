f8d7f62c1225c7726f21c4acde8a5644a8b3a5d5
Kauly <kauly.leite@icloud.com>
2023-07-11 20:10:30 -0300

    feat: Definindo o tamanho máximo para os campos na página de pedidos. Implementando a seleção do estado em campo.

--- app/static/js/scripts_pedidos.js
+++ app/static/js/scripts_pedidos.js
@@ -28,6 +28,48 @@
         });
 })();
 
+(function () {
+    'use strict';
+    const estados = [
+        {nome: 'Acre', sigla: 'AC'},
+        {nome: 'Alagoas', sigla: 'AL'},
+        {nome: 'Amapá', sigla: 'AP'},
+        {nome: 'Amazonas', sigla: 'AM'},
+        {nome: 'Bahia', sigla: 'BA'},
+        {nome: 'Ceará', sigla: 'CE'},
+        {nome: 'Distrito Federal', sigla: 'DF'},
+        {nome: 'Espírito Santo', sigla: 'ES'},
+        {nome: 'Goiás', sigla: 'GO'},
+        {nome: 'Maranhão', sigla: 'MA'},
+        {nome: 'Mato Grosso', sigla: 'MT'},
+        {nome: 'Mato Grosso do Sul', sigla: 'MS'},
+        {nome: 'Minas Gerais', sigla: 'MG'},
+        {nome: 'Pará', sigla: 'PA'},
+        {nome: 'Paraíba', sigla: 'PB'},
+        {nome: 'Paraná', sigla: 'PR'},
+        {nome: 'Pernambuco', sigla: 'PE'},
+        {nome: 'Piauí', sigla: 'PI'},
+        {nome: 'Rio de Janeiro', sigla: 'RJ'},
+        {nome: 'Rio Grande do Norte', sigla: 'RN'},
+        {nome: 'Rio Grande do Sul', sigla: 'RS'},
+        {nome: 'Rondônia', sigla: 'RO'},
+        {nome: 'Roraima', sigla: 'RR'},
+        {nome: 'Santa Catarina', sigla: 'SC'},
+        {nome: 'São Paulo', sigla: 'SP'},
+        {nome: 'Sergipe', sigla: 'SE'},
+        {nome: 'Tocantins', sigla: 'TO'}
+    ];
+
+    const input = document.getElementById('estado');
+
+    estados.forEach(function (estado) {
+        const option = document.createElement('option');
+        option.value = estado.sigla;
+        option.textContent = estado.nome;
+        input.appendChild(option);
+    });
+})();
+
 function validarQtde(input) {
     if (input.value.length > 2) {
         input.value = input.value.slice(0, 2);
