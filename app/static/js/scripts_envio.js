b6cd66f6106a13b8c433147c0ad6637656762f81
Kauly <kauly.leite@icloud.com>
2023-07-11 20:34:50 -0300

    fix: Tratando de utilizar o número do pedido no título da página gerada em .pdf.

--- app/static/js/scripts_envio.js
+++ app/static/js/scripts_envio.js
@@ -1,6 +1,6 @@
-function gerar(id) {
+function gerar(id, pedido) {
     let conteudo = document.getElementById(id).innerHTML;
-    let nome = 'Pedido - {{ pedido }}';
+    let nome = 'Pedido - ' + pedido;
     let iframe = document.createElement('iframe');
     let bootstrap = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"' +
         ' rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"' +
